b6742d1645d66a107957d366fa23c43dbfa6f9ac
trung-2b <trungnt256@gmail.com>
2020-08-14 14:53:31 +0700

    18. Webpack eslint loader

---
+++ 18. Webpack eslint loader/webpack-demo/src/index.js
@@ -0,0 +1,2 @@
+var author = 'kentrung'
+console.log('Webpack từ A đến Á cùng ' + author)
---
+++ 18. Webpack eslint loader/webpack-demo/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+    ],
+  },
+}
